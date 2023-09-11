661c6769a158655b919fa873d19d2d1782544cc7
ngf93 <nabiullinaguzel93@gmail.com>
2023-09-11 17:00:17 +0300

    fix

--- src/components/svg/FolderSvg3.jsx
+++ src/components/svg/FolderSvg3.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const FolderSvg3 = ({className}) => {
   return (
-    <svg className={className} preserveAspectRatio="none" width="405" height="500" viewBox="0 0 405 500" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M405 57.6C405 46.5543 396.046 37.6 385 37.6H277.54C272.085 37.6 266.867 35.3717 263.094 31.4314L238.906 6.16857C235.133 2.22829 229.915 0 224.46 0H80.3698H20C8.95432 0 0 8.9543 0 20V480C0 491.046 8.95431 500 20 500H385C396.046 500 405 491.046 405 480V57.6Z" fill="white"/>
+    <svg className={className} preserveAspectRatio="none" width="405" height="501" viewBox="0 0 405 501" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M1 20.6689V480.669C1 491.162 9.50659 499.669 20 499.669H385C395.493 499.669 404 491.162 404 480.669V58.2689C404 47.7755 395.493 39.2689 385 39.2689H277.54C271.812 39.2689 266.333 36.9292 262.372 32.7919L238.184 7.52909C234.6 3.78582 229.642 1.66895 224.46 1.66895H80.3698H20C9.50659 1.66895 1 10.1755 1 20.6689Z" stroke="transparent" strokeWidth="2"/>
     </svg>
+    
   )
 }
 
