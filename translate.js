b386aef2016726824d5b1908a99bed0d3186e148
Fatih <110812530+ManapovFatih@users.noreply.github.com>
2024-07-11 12:59:13 +0300

    Update translate.js

--- translate.js
+++ translate.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra');
 const path = require('path');
 
 
-const directoryPath = './src/pages'; // Укажите путь к директории с файлами
+const directoryPath = './src'; // Укажите путь к директории с файлами
 
 let i18nContent = {};
 const ignoredComments = []; // Список комментариев, которые не нужно трогать
@@ -11,10 +11,13 @@ const savedComments = [];
 const savedSentences = [];
 const savedWords = [];
 const ignoredDirectories = ['assets', 'config', 'contexts', 'hooks', 'layouts', 'locales', 'providers', 'routes', 'services']; // Папки, которые нужно игнорировать
-const i18nPath = path.join(__dirname, 'i18n.json');
-if (fs.existsSync(i18nPath)) {
-    i18nContent = JSON.parse(fs.readFileSync(i18nPath, 'utf8'));
+const RuPath = path.join(__dirname, 'ru.json');
+if (fs.existsSync(RuPath)) {
+    i18nContent = JSON.parse(fs.readFileSync(RuPath, 'utf8'));
 }
+const i18nPath = path.join(__dirname, 'i18n.json');
+
+
 
 
 function readFiles(directoryPath) {
@@ -57,16 +60,37 @@ function readFiles(directoryPath) {
                 ignoredSentences.push(...tSentences);
             }
 
+            const lines = content.split('\n'); // Разделяем строку на отдельные строки
+
+            const result = [];
+
+            for (const line of lines) { // Проходим по каждой строке
+                const russianWords = line.match(/(?:(["'])[^"']*?\1)|(?:(>([^<]*<))|(?:(\[[^\]]*\]))|(?:(\([^)]*\))))/gi);
+
+                if (russianWords) {
+                    ;
+                    for (const match of russianWords) {
+                        if (match) {
+                            const russianMatch = match.match(/[А-Яа-яЁё]+/);
+                            if (russianMatch) {
+                                result.push(match.trim());
+                            }
+                        }
+                    }
+                }
+            }
+
+            // const russianWords = content.match(/(?:(["'])[^"']*?\1)|(?:(>([^<]*<))|(?:(\[[^\]]*\]))|(?:(\([^)]*\))))/gi);
 
 
+            console.log(result);
 
-
-            const russianWords = content.match(/([>"'()\[\]]*)(?:[0-9]*?[a-zA-Z]*[А-Яа-яЁё]+[А-Яа-яЁё]*[\sА-Яа-яЁё]+?[\s0-9]*?[\sa-zA-Z]*?[\s,.?!«»%()\]\-_'’"\s]+[/sА-Яа-яЁё]*?[,.?!«»%()<\]\-_'’"0-9a-zA-Z\s]*)+/gis);
+            // const russianWords = content.match(/([>"'()\[\]]*)(?:[0-9]*?[a-zA-Z]*[А-Яа-яЁё]+[А-Яа-яЁё]*[\sА-Яа-яЁё]+?[\s0-9]*?[\sa-zA-Z]*?[\s,.?!«»%()\]\-_'’"\s]+[/sА-Яа-яЁё]*?[,.?!«»%()<\]\-_'’"0-9a-zA-Z\s]*)+/gis);
 
             // const russianWords = content.match(/(?:["'(\[\]]*)([а-яё]+[-а-яё]*[\sА-ЯЁ]+[-а-яё]*?[,.?!«»()\-_'’"\s]*)+/gis); // Machine worked
-            if (russianWords) {
+            if (result) {
                 // Сохраняем комментарии и их позиции
-                russianWords.forEach((words) => {
+                result.forEach((words) => {
                     let word = words.trim();
                     const startIndexWord = content.indexOf(word);
                     savedWords.push({ word, startIndexWord });
@@ -75,11 +99,13 @@ function readFiles(directoryPath) {
             }
             savedWords.forEach(({ word, startIndexWord }) => {
                 let newWord;
+                let saveRu = [];
                 if (word.startsWith('(')) {
                     newWord = word.replace(/^\(|\)$/g, ''); // Удаляем скобки в начале и конце строки
                     newWord = newWord.trim(); // Удаляем пробелы в начале и конце
                     newWord = newWord.replace(/^["']|["']$/g, ''); // Удаляем кавычки
                     newWord = newWord.trim(); // Удаляем пробелы
+                    saveRu.push(newWord);
                     newWord = `(t('${newWord}'))`;
                 }
                 else if (word.startsWith('[')) {
@@ -88,22 +114,28 @@ function readFiles(directoryPath) {
                     newWord = newWord.replace(/^["']|["']$/g, ''); // Удаляем кавычки
                     newWord = newWord.trim();
                     if (newWord.includes('", "')) {
-                        newWord = newWord.split('", "').map(item => `t('${item.trim()}')`).join(', ');
-                        newWord = `[${newWord}`;
+                        newWord = newWord.split('", "').map(item => {
+                            saveRu.push(item.trim());
+                            return `t('${item.trim()}')`;
+                        }).join(', ');
+                        newWord = `[${newWord}]`;
                     }
                     else {
-                        newWord = `[t('${newWord}')`;
+                        saveRu.push(newWord);
+                        newWord = `[t('${newWord}')]`;
                     }
                 }
                 else if (word.startsWith('>')) {
-                    newWord = word.trim().replace(/^[>]|[<]$/g, '');
+                    newWord = word.replace(/^[>]|[<]$/g, '');
                     newWord = newWord.trim();
-                    newWord = `>{t('${newWord}')}`;
+                    saveRu.push(newWord);
+                    newWord = `>{t('${newWord}')}<`;
 
                 }
                 else {
                     newWord = word.replace(/^["']|["']$/g, '');
                     newWord = newWord.trim();
+                    saveRu.push(newWord);
                     newWord = `{t('${newWord}')}`;
 
                 }
@@ -111,9 +143,12 @@ function readFiles(directoryPath) {
                 content = content.replace(`{startIndexWord[${startIndexWord}]}`, newWord);
 
 
-                if (!i18nContent[newWord]) {
-                    i18nContent[newWord] = newWord;
-                }
+                saveRu.forEach((e) => {
+                    if (!i18nContent[e]) {
+                        i18nContent[e] = e;
+                    }
+                });
+
 
             });
             savedComments.forEach(({ comment, startIndexComments }) => {
@@ -129,7 +164,8 @@ function readFiles(directoryPath) {
 
 readFiles(directoryPath);
 // Создаем файл i18n
+const valuesArray = Object.values(i18nContent);
 
-fs.writeFileSync(i18nPath, JSON.stringify(i18nContent, null, 2));
-
+fs.writeFileSync(RuPath, JSON.stringify(i18nContent, null, 2));
+fs.writeFileSync(i18nPath, JSON.stringify(valuesArray, null, 2));
 console.log('Скрипт выполнен успешно.');
